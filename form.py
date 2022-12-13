62400705b5974c3ac638a6934c203ae7a8f275c7
FujinoKeito <kd1335001@st.kobedenshi.ac.jp>
2022-12-13 12:08:20 +0900

    ボタン押下でattend_chrome.pyを実行

--- form.py
+++ form.py
@@ -1,6 +1,11 @@
 from tkinter import *
 from tkinter import ttk
+import subprocess
 import datetime
+
+#Pythonファイル読み込み.
+def execute_py():
+    subprocess.call("python attend_chrome.py")
 
 #現在時刻取得.
 dt_now = datetime.datetime.now()
@@ -12,19 +17,21 @@ END = datetime.time(14, 50, 0)
 
 root = Tk()
 root.title("自動登校登録")#タイトル.
+root.geometry("300x200")
 
 if(AM <= now <PM):#午前登録.
-    label_1 = ttk.Label(root,text ='登校登録をします')
+    label_1 = ttk.Label(root,text ='登校登録',font=("ヒラギノ角ゴ", "20", "normal"))
 elif(PM <= now <END):#午後登録.
-    label_1 = ttk.Label(root,text ='中間登録をします')
+    label_1 = ttk.Label(root,text ='中間登録',font=("ヒラギノ角ゴ", "20", "normal"))
 elif(END <= now):#下校登録.
-    label_1 = ttk.Label(root,text ='下校登録をします')
+    label_1 = ttk.Label(root,text ='下校登録',font=("ヒラギノ角ゴ", "20", "normal"))
 else:#時間外.
-    label_1 = ttk.Label(root,text ='時間外です')
+    label_1 = ttk.Label(root,text ='時間外です',font=("ヒラギノ角ゴ", "20", "normal"))
 
-button_1 = ttk.Button(root,text = 'OK',command=lambda:())
+button_1 = ttk.Button(root,text = 'OK', padding=15, command=execute_py)
 
-#レイアウト
+#レイアウト.
+label_1.place(x=100,y=100)
 label_1.pack()
 button_1.pack()
 
